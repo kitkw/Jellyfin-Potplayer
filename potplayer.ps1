9504df19ae22ab080943c49065dd23607fe65ce9
kitkw <31963066+kitkw@users.noreply.github.com>
2022-02-28 08:29:29 +0800

    Update potplayer.ps1

--- potplayer.ps1
+++ potplayer.ps1
@@ -3,4 +3,4 @@ $path=$args[0]
 $path=$path -replace "potplayer://" , ""
 $path= [System.Web.HttpUtility]::UrlDecode($path)
 echo $path
-& "C:\\Program Files\\DAUM\\PotPlayer\\PotPlayerMini64.exe" $path
+& "D:\\Program Files\\DAUM\\PotPlayer\\PotPlayerMini64.exe" $path
